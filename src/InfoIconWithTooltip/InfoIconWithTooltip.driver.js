4c939c6c5eba4263ca0d0de6bdbb13610ab8e535
Iurii Komarov <iuriikomarov@icloud.com>
2017-12-18 17:24:42 +0200

    Unit tests covering mouseover/mouseout

--- src/InfoIconWithTooltip/InfoIconWithTooltip.driver.js
+++ src/InfoIconWithTooltip/InfoIconWithTooltip.driver.js
@@ -1,4 +1,18 @@
-// import React from 'react';
-const infoIconWithTooltipDriverFactory = () => {};
+import ReactTestUtils from 'react-dom/test-utils';
 
-export default infoIconWithTooltipDriverFactory;
+export default ({element, wrapper}) => {
+  const root = {
+    $: query => (document.body.querySelector(query) || (wrapper.querySelector && wrapper.querySelector(query))),
+    $$: query => {
+      const documentResult = document.body.querySelectorAll(query);
+      return documentResult.length > 0 ? documentResult : (wrapper.querySelectorAll && wrapper.querySelectorAll(query));
+    }
+  };
+
+  return {
+    isShown: () => !!root.$('.tooltip'),
+    mouseEnter: () => ReactTestUtils.Simulate.mouseEnter(element),
+    mouseLeave: () => ReactTestUtils.Simulate.mouseLeave(element)
+  };
+};
+
--- src/InfoIconWithTooltip/InfoIconWithTooltip.spec.js
+++ src/InfoIconWithTooltip/InfoIconWithTooltip.spec.js
@@ -1,1 +1,51 @@
-describe('InfoIconWithTooltip', () => {});
+import React from 'react';
+import {createDriverFactory} from '../test-common';
+import InfoIconWithTooltipDriver from './InfoIconWithTooltip.driver';
+import InfoIconWithTooltip from './InfoIconWithTooltip';
+
+function resolveIn(timeout) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({});
+    }, timeout);
+  });
+}
+
+describe('InfoIconWithTooltip', () => {
+  const createDriver = () =>
+    createDriverFactory(InfoIconWithTooltipDriver)(
+      <InfoIconWithTooltip {...{showDelay: 5, hideDelay: 5, content: 'Foo bar baz'}}/>
+    );
+
+  beforeEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  it('should be hidden by default', () => {
+    const driver = createDriver();
+    expect(driver.isShown()).toBeFalsy();
+  });
+
+  it('should show a tooltip once hovering', () => {
+    const driver = createDriver();
+    driver.mouseEnter();
+    expect(driver.isShown()).toBeFalsy();
+    return resolveIn(30).then(() => {
+      expect(driver.isShown()).toBeTruthy();
+    });
+  });
+
+  it('should hide when mouse leaving', () => {
+    const driver = createDriver();
+    driver.mouseEnter();
+    expect(driver.isShown()).toBeFalsy();
+    return resolveIn(30).then(() => {
+      expect(driver.isShown()).toBeTruthy();
+      driver.mouseLeave();
+      return resolveIn(30).then(() => {
+        expect(driver.isShown()).toBeFalsy();
+      });
+    });
+  });
+
+});
