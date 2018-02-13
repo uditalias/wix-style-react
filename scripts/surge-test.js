e18fedcfee0df21eff35dd7553e4d66e4107c772
Shilo Mangam <smangam@gmail.com>
2018-02-13 16:12:02 +0200

    test

--- scripts/surge-test.js
+++ scripts/surge-test.js
@@ -1,10 +1,10 @@
 const {spawn} = require('child_process');
-const https = require('https');
+// const https = require('https');
 const {
   TRAVIS_REPO_SLUG: travisRepoSlug,
   STORYBOOK_DIST: storybookDist,
-  TRAVIS_PULL_REQUEST: travisPullRequest,
-  GITHUB_API_TOKEN: githubApiToken
+  TRAVIS_PULL_REQUEST: travisPullRequest
+  // GITHUB_API_TOKEN: githubApiToken
 } = process.env;
 
 
@@ -13,36 +13,38 @@ const deployPath = `/home/travis/build/${repoOwner}/${repoName}/${storybookDist}
 
 const deploySubdomain = `pr-${travisPullRequest}`;
 const deployDomain = `https://${repoOwner}-${repoName}-${deploySubdomain}.surge.sh`;
+const str = `node ${process.cwd()}/node_modules/.bin/surge --project ${deployPath} --domain ${deployDomain}`;
 
-const childProcess = spawn(`node ' + ${process.cwd()} + '/node_modules/.bin/surge --project ${deployPath} --domain ${deployDomain}`);
+const childProcess = spawn(str);
 childProcess.on('close', () => {
+  console.log(str);
   console.log('ON CLOSE');
 });
-childProcess.on('error', () => {
-  console.log('ON ERROR');
+childProcess.on('error', (e) => {
+  console.log(e, 'ON ERROR');
 });
 
-
-const githubCommentsPath = `/repos/${travisRepoSlug}/issues/${travisPullRequest}/comments`;
-const githubCommentsData = {
-  body: `View storybook at: ${deployDomain}`
-};
-
-const options = {
-  hostname: 'api.github.com',
-  path: githubCommentsPath,
-  port: 443,
-  method: 'POST',
-  data: JSON.stringify(githubCommentsData),
-  headers: {
-    Authorization: `token ${githubApiToken}`
-  }
-};
-try {
-  https.request(options, () => {
-    console.log('Success');
-  });
-} catch (e) {
-  console.error(e, 'ERROR IN GITHUB COMMENTS');
-}
+//
+// const githubCommentsPath = `/repos/${travisRepoSlug}/issues/${travisPullRequest}/comments`;
+// const githubCommentsData = {
+//   body: `View storybook at: ${deployDomain}`
+// };
+//
+// const options = {
+//   hostname: 'api.github.com',
+//   path: githubCommentsPath,
+//   port: 443,
+//   method: 'POST',
+//   data: JSON.stringify(githubCommentsData),
+//   headers: {
+//     Authorization: `token ${githubApiToken}`
+//   }
+// };
+// try {
+//   https.request(options, () => {
+//     console.log('Success');
+//   });
+// } catch (e) {
+//   console.error(e, 'ERROR IN GITHUB COMMENTS');
+// }
 
