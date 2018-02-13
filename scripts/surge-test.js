1dc67cbdc4b4f2966f3dd2e84518dac37605feb4
Shilo Mangam <smangam@gmail.com>
2018-02-13 16:24:43 +0200

    surge fix

--- scripts/surge-test.js
+++ scripts/surge-test.js
@@ -13,15 +13,10 @@ const deployPath = `/home/travis/build/${repoOwner}/${repoName}/${storybookDist}
 
 const deploySubdomain = `pr-${travisPullRequest}`;
 const deployDomain = `https://${repoOwner}-${repoName}-${deploySubdomain}.surge.sh`;
-const str = ` --project ${deployPath} --domain ${deployDomain}`;
 
 const childProcess = spawn('node', [`${process.cwd()}/node_modules/.bin/surge`, '--project', deployPath, '--domain', deployDomain]);
-childProcess.on('close', () => {
-  console.log(str);
-  console.log('ON CLOSE');
-});
-childProcess.on('error', (e) => {
-  console.log(e, 'ON ERROR');
+childProcess.on('error', e => {
+  console.error('Error on surge process', e);
 });
 
 //
