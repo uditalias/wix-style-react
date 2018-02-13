2f2fbe4f33163b2a31a12beb41c774349f67a179
Shilo Mangam <smangam@gmail.com>
2018-02-13 16:47:19 +0200

    surge output

--- scripts/surge-test.js
+++ scripts/surge-test.js
@@ -14,13 +14,22 @@ const deployPath = `/home/travis/build/${repoOwner}/${repoName}/${storybookDist}
 const deploySubdomain = `pr-${travisPullRequest}`;
 const deployDomain = `https://${repoOwner}-${repoName}-${deploySubdomain}.surge.sh`;
 
+let surgeResults = '';
+
 const childProcess = spawn('node', [`${process.cwd()}/node_modules/.bin/surge`, '--project', deployPath, '--domain', deployDomain]);
-childProcess.on('data', data => {
-  console.log(data);
+
+childProcess.stdout.on('data', data => {
+  surgeResults += data;
 });
+
 childProcess.on('error', e => {
   console.error('Error on surge process', e);
 });
+
+childProcess.on('close', () => {
+  console.log(surgeResults);
+});
+
 
 //
 // const githubCommentsPath = `/repos/${travisRepoSlug}/issues/${travisPullRequest}/comments`;
