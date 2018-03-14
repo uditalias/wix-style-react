3294eb9a57abec8f48eedd3f157fe2642b026229
Erez Makavy <erez@wix.com>
2018-03-14 17:32:21 +0200

    Add focus first last states

--- src/Composite/FieldWithSelectionComposite/FieldWithSelectionComposite.js
+++ src/Composite/FieldWithSelectionComposite/FieldWithSelectionComposite.js
@@ -8,13 +8,17 @@ import FieldLabelAttributes from '../../FieldLabelAttributes/FieldLabelAttribute
 class FieldWithSelectionComposite extends WixComponent {
   constructor(props) {
     super(props);
-    this._onTextInputFocus = this._onTextInputFocus.bind(this);
-    this._onTextInputBlur = this._onTextInputBlur.bind(this);
+    this._onFocusFirst = this._onFocusFirst.bind(this);
+    this._onBlurFirst = this._onBlurFirst.bind(this);
+    this._onFocusLast = this._onFocusLast.bind(this);
+    this._onBlurLast = this._onBlurLast.bind(this);
 
     this.state = {
-      textInputFocused: false
+      hasFocusFirst: false,
+      hasFocusLast: false
     };
   }
+
   getPrototypeDisplayName(selectionInput) {
     const type = Object.getPrototypeOf(selectionInput.type);
     return type && type.displayName;
@@ -28,14 +32,23 @@ class FieldWithSelectionComposite extends WixComponent {
     return (this.props.children.length === 3) ? this.props.children[1] : this.props.children[0];
   }
 
-  _onTextInputFocus() {
-    this.setState({textInputFocused: true});
+  _onFocusFirst() {
+    this.setState({hasFocusFirst: true});
   }
 
-  _onTextInputBlur(e) {
+  _onBlurFirst(e) {
     const textInput = this._getTextInput();
-    textInput.props.onBlur && textInput.props.onBlur(e);
-    this.setState({textInputFocused: false});
+    this.setState({hasFocusFirst: false}, () => {
+      textInput.props.onBlur && textInput.props.onBlur(e);
+    });
+  }
+
+  _onFocusLast() {
+    this.setState({hasFocusLast: true});
+  }
+
+  _onBlurLast() {
+    this.setState({hasFocusLast: false});
   }
 
   render() {
@@ -54,7 +67,8 @@ class FieldWithSelectionComposite extends WixComponent {
       inputsWrapperClassNames[styles[selectionInputType.toLowerCase()]] = true;
     }
 
-    inputsWrapperClassNames[styles.focused] = this.state.textInputFocused;
+    inputsWrapperClassNames[styles.hasFocusFirst] = this.state.hasFocusFirst;
+    inputsWrapperClassNames[styles.hasFocusLast] = this.state.hasFocusLast;
     inputsWrapperClassNames[styles.error] = this.props.error;
     inputsWrapperClassNames[styles.disabled] = this.props.disabled;
 
@@ -62,8 +76,20 @@ class FieldWithSelectionComposite extends WixComponent {
       <div className={styles.wrapper} >
         {label}
         <div className={classNames(inputsWrapperClassNames)}>
-          {React.cloneElement(textInput, {onFocus: this._onTextInputFocus, onBlur: this._onTextInputBlur, error: this.props.error, disabled: this.props.disabled, withSelection: true})}
-          {React.cloneElement(selectionInput, {noBorder: true, noRightBorderRadius: true, disabled: this.props.disabled})}
+          {React.cloneElement(textInput, {
+            onFocus: this._onFocusFirst,
+            onBlur: this._onBlurFirst,
+            error: this.props.error,
+            disabled: this.props.disabled,
+            withSelection: true
+          })}
+          {React.cloneElement(selectionInput, {
+            noBorder: true,
+            noRightBorderRadius: true,
+            disabled: this.props.disabled,
+            onFocus: this._onFocusLast,
+            onBlur: this._onBlurLast
+          })}
         </div>
       </div>
     );
